eac662b0e4ebfec598d5482561deb7785e51936b
Anish <anishawasthi16@gmail.com>
2024-05-31 16:02:00 +0530

    added user authentication using clerk sdk

--- flashflow/components/Navbar.tsx
+++ flashflow/components/Navbar.tsx
@@ -2,11 +2,11 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import MobileNav from "./MobileNav";
+import { SignedIn, UserButton } from "@clerk/nextjs";
 
 const Navbar = () => {
   return (
     <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
-
       <Link href="/" className="flex items-center gap-1">
         <Image
           src="/icons/logo.svg"
@@ -21,10 +21,11 @@ const Navbar = () => {
       </Link>
 
       <div className="flex-between gap-5">
-        {/* CLERK USER MANAGEMENT */}
-        <MobileNav/>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        <MobileNav />
       </div>
-
     </nav>
   );
 };
