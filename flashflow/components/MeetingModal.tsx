04840c730b1f0f3afca1fe245bc1dff17a56474f
Anish <anishawasthi16@gmail.com>
2024-05-31 23:59:43 +0530

    creating home hero page and quick actions ui on the hero page and modal component

---
+++ flashflow/components/MeetingModal.tsx
@@ -0,0 +1,60 @@
+import React, { ReactNode } from "react";
+import { Dialog, DialogContent } from "@/components/ui/dialog";
+import { defaultOverrides } from "next/dist/server/require-hook";
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+import { Button } from "./ui/button";
+
+interface modalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  title: string;
+  buttonText?: string;
+  className?: string;
+  handleClick?: () => void;
+  children?: ReactNode;
+  image?: string;
+  buttonIcon?: string;
+}
+const MeetingModal = ({
+  isOpen,
+  onClose,
+  title,
+  buttonText,
+  className,
+  handleClick,
+  children,
+  buttonIcon,
+  image,
+}: modalProps) => {
+  return (
+    <Dialog open={isOpen} onOpenChange={onClose}>
+      <DialogContent className="flex flex-col max-w-[520px] bg-dark-1 w-full border-none gap-6 px-6   py-9 text-white">
+        <div className="flex flex-col gap-6">
+          {image && (
+            <div className="flex justify-center">
+              <Image src={image} alt="image" width={72} height={72} />
+            </div>
+          )}
+
+          <h1 className={cn("text-3xl font-bold", className)}>{title}</h1>
+
+          {children}
+
+          <Button
+            className="bg-blue-1 focus-visible:ring-0 focus-visible:ring-offset-0"
+            onClick={handleClick}
+          >
+            {buttonIcon && (
+              <Image src={buttonIcon} alt="buttonIcon" width={72} height={72} />
+            )}
+            &nbsp;
+            {buttonText || "Default Text"}
+          </Button>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default MeetingModal;
